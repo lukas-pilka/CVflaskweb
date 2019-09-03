85abc539e76629b670c558cedc0d8aa4e7b631c1
Lukas Pilka <pilka@blueghost.cz>
2019-09-03 19:02:42 +0200

    Pridava vypocet doby udalosti a doby cele historie

--- date.py
+++ date.py
@@ -7,17 +7,32 @@ db = firestore.Client()
 collection_ref = db.collection(u'cvitems').get()
 cvitems = list(doc.to_dict() for doc in collection_ref)
 
-now = datetime.datetime.now()
-now = now.strftime("%Y, %m, %d")
+# counts days between dateFrom and dateTo and adds sum to dict
+
+now = datetime.date.today()
 
 for i in cvitems:
     for event in i.values():
         dateFrom = event.get('dateFrom', 0)
-        dateTo = event.get('dateTo', now)
-        print(dateFrom, dateTo)
+        dateFrom = dateFrom.date()
+        dateTo = event.get('dateTo')
+        if dateTo != None:
+            dateTo = dateTo.date()
+        else:
+            dateTo = now
+        daysCount = dateTo - dateFrom
+        event["dayscount"] = daysCount.days
 
+historyStartDate = now
+for i in cvitems:
+    for event in i.values():
+        dateFrom = event.get('dateFrom', now)
+        dateFrom = dateFrom.date()
+        if dateFrom < historyStartDate:
+            historyStartDate = dateFrom
 
-dateFrom = date(2008, 8, 18)
-dateTo = date(2008, 9, 18)
-delta = dateTo - dateFrom
-print(delta.days)
+print(historyStartDate)
+
+HistoryDaysCount = now - historyStartDate
+HistoryDaysCount = HistoryDaysCount.days
+print(HistoryDaysCount)
--- main.py
+++ main.py
@@ -4,6 +4,39 @@ db = firestore.Client()
 collection_ref = db.collection(u'cvitems').get()
 cvitems = list(doc.to_dict() for doc in collection_ref)
 
+
+# counts days between dateFrom and dateTo and adds sum to dict
+import datetime
+
+now = datetime.date.today()
+
+for i in cvitems:
+    for event in i.values():
+        dateFrom = event.get('dateFrom', 0)
+        dateFrom = dateFrom.date()
+        dateTo = event.get('dateTo')
+        if dateTo != None:
+            dateTo = dateTo.date()
+        else:
+            dateTo = now
+        daysCount = dateTo - dateFrom
+        event["dayscount"] = daysCount.days
+
+# Searches from oldest dateFrom and sets like a history start date
+
+historyStartDate = now
+for i in cvitems:
+    for event in i.values():
+        dateFrom = event.get('dateFrom', now)
+        dateFrom = dateFrom.date()
+        if dateFrom < historyStartDate:
+            historyStartDate = dateFrom
+
+HistoryDaysCount = now - historyStartDate
+HistoryDaysCount = HistoryDaysCount.days
+
+# flask routing
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,7 +44,7 @@ app = Flask(__name__)
 @app.route('/')
 def root():
 
-    return render_template('index.html', cvitems=cvitems)
+    return render_template('index.html', cvitems=cvitems, historyStartDate=historyStartDate, now=now, HistoryDaysCount=HistoryDaysCount)
 
 
 if __name__ == '__main__':
